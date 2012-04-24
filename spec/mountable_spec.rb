dfd9c3a22ed43794053a9654a916c974f3de2372
Eric Oestrich <eric@oestrich.org>
2012-04-24 13:54:41 -0500

    Test mounting Raddocs

---
+++ spec/mountable_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "Mountable" do
+  before do
+    Capybara.app = Rack::Builder.new do
+      map "/docs" do
+        run Raddocs
+      end
+    end
+
+    visit "/docs"
+  end
+
+  it "should show the index page" do
+    page.should have_content("Api Documentation")
+  end
+
+  it "should update the links" do
+    click_link "Creating an order"
+
+    current_path.should == "/docs/orders/creating_an_order"
+    page.should have_content("Orders API")
+  end
+end
